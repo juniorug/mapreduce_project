bbdea210c017d93d2a64175a16756ee3422c1b4e
Junior@mac <juniorug@gmail.com>
2016-04-25 01:54:06 -0300

    primeiro job

---
+++ src/main/java/main/BrazilWordCounter.java
@@ -0,0 +1,39 @@
+package main;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import mappers.BrazilWordCountMapper;
+import reducers.IntSumReducer;
+
+
+
+public class BrazilWordCounter {
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: BrazilWordCounter <in> <out>");
+			System.exit(2);
+		}
+		Job job = Job.getInstance(conf, "StackOverflow Comment Word Count");
+		job.setJarByClass(BrazilWordCounter.class);
+		job.setMapperClass(BrazilWordCountMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+}
---
+++ src/main/java/mappers/BrazilWordCountMapper.java
@@ -0,0 +1,48 @@
+package mappers;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import util.MRDPUtils;
+
+public class BrazilWordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+		// Parse the input string into a nice map
+		Map<String, String> parsed = MRDPUtils.transformXmlToMap(value.toString());
+
+		// Grab the "Text" field, since that is what we are counting over
+		String txt = parsed.get("Text");
+
+		// .get will return null if the key is not there
+		if (txt == null) {
+			// skip this record
+			return;
+		}
+
+		// Unescape the HTML because the SO data is escaped.
+		txt = StringEscapeUtils.unescapeHtml(txt.toLowerCase());
+
+		// Remove some annoying punctuation
+		txt = txt.replaceAll("'", ""); // remove single quotes (e.g., can't)
+		txt = txt.replaceAll("[^a-zA-Z]", " "); // replace the rest with a
+		// space
+
+		// Tokenize the string, then send the tokens away
+		StringTokenizer itr = new StringTokenizer(txt);
+		while (itr.hasMoreTokens()) {
+			word.set(itr.nextToken());
+			context.write(word, one);
+		}
+	}
+}
