22dcde2009f8bbe24de8f1bae0e82dde9f019d7c
Junior@mac <juniorug@gmail.com>
2016-04-29 04:06:13 -0300

    questao2 B

--- src/main/java/reducers/UserJoinReducer.java
+++ src/main/java/reducers/UserJoinReducer.java
@@ -2,34 +2,33 @@ package reducers;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class UserJoinReducer extends Reducer<Text, Text, Text, Text> {
 
-    private Text tmp = new Text();
     private ArrayList<Text> listA = new ArrayList<Text>();
     private ArrayList<Text> listB = new ArrayList<Text>();
-
-    public void reduce(Text key, Iterator<Text> values, Context context) throws IOException, InterruptedException {
-
+        
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        
         listA.clear();
         listB.clear();
-
-        while (values.hasNext()) {
-            tmp = values.next();
-            if (tmp.charAt(0) == 'A') {
+        
+        for (Text tmp : values) {
+            if (tmp.toString().charAt(0) == 'A') {
                 listA.add(new Text(tmp.toString().substring(1)));
-            } else if (tmp.charAt('0') == 'B') {
+            } else if (tmp.toString().charAt(0) == 'B') {
                 listB.add(new Text(tmp.toString().substring(1)));
             }
+
         }
         executeJoinLogic(context);
     }
 
-    private void executeJoinLogic(Context context) throws IOException, InterruptedException {
+    private void executeJoinLogic(Context context) throws IOException,
+            InterruptedException {
         if (!listA.isEmpty() && !listB.isEmpty()) {
             for (Text A : listA) {
                 for (Text B : listB) {
@@ -39,4 +38,5 @@ public class UserJoinReducer extends Reducer<Text, Text, Text, Text> {
         }
     }
 
-}
+
+}   
